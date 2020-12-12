b6555e8bbcdff202d0d7853565e8343354e811bd
keigo9 <keigo.matsunari@gmail.com>
2020-12-12 11:05:32 +0900

    MemberPersonコンポーネント作成

--- src/views/Member.vue
+++ src/views/Member.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
+  <div class="member">
     <h1>This is an member page</h1>
+    <MemberPerson />
   </div>
 </template>
+
+<script>
+import MemberPerson from "@/components/MemberPerson.vue";
+
+export default {
+  name: "Member",
+  components: {
+    MemberPerson
+  }
+};
+</script>
