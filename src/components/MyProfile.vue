a71f05818def6aa5732666e4ea401d96db0493c2
keigo9 <keigo.matsunari@gmail.com>
2020-12-13 15:45:09 +0900

    memberページ、コンポーネント作成

---
+++ src/components/MyProfile.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card max-width="1000" class="mx-auto">
+    <v-img :src="src" height="300px" dark>
+      <v-row class="fill-height">
+        <v-card-title>
+          <v-btn dark icon class="mr-4">
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+
+          <v-btn dark icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-spacer></v-spacer>
+
+        <v-card-title class="white--text pl-12 pt-12">
+          <div class="display-1 pl-12 pt-12">
+            {{ name }}
+          </div>
+        </v-card-title>
+      </v-row>
+    </v-img>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Ali Conners",
+      src: require("@/assets/ali.png")
+      // src: "https://cdn.vuetifyjs.com/images/lists/ali.png"
+    };
+  }
+};
+</script>
--- src/views/Member.vue
+++ src/views/Member.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="member">
-    <h1>This is an member page</h1>
+    <MyProfile />
     <MemberPerson />
+    <MemberFooter />
   </div>
 </template>
 
 <script>
+import MyProfile from "@/components/MyProfile.vue";
 import MemberPerson from "@/components/MemberPerson.vue";
+import MemberFooter from "@/components/MemberFooter.vue";
 
 export default {
   name: "Member",
   components: {
-    MemberPerson
+    MyProfile,
+    MemberPerson,
+    MemberFooter
   }
 };
 </script>
