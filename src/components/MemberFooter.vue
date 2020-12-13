014d9f43b3ddd1b3fa01b2afb40ef206760c9cd7
keigo9 <keigo.matsunari@gmail.com>
2020-12-13 15:44:28 +0900

    footer作成

---
+++ src/components/MemberFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-footer color="blue" padless>
+    <v-row class="flex" justify="center">
+      <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+        <v-icon size="24px">
+          {{ icon }}
+        </v-icon>
+      </v-btn>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"]
+  })
+};
+</script>
