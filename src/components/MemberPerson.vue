cac132df0b4cae18a351c8c8d38eddeea90f0e39
keigo9 <keigo.matsunari@gmail.com>
2020-12-22 23:23:21 +0900

    メンバー編集ボタン追加

--- src/components/MemberPerson.vue
+++ src/components/MemberPerson.vue
@@ -1,19 +1,38 @@
 <template>
   <v-card max-width="1000" class="mx-auto">
+    <v-row
+      align="center"
+      justify="space-around"
+      class="v-row"
+      @click="isEdit()"
+    >
+      <v-btn tile color="success">
+        <v-icon left>
+          mdi-pencil
+        </v-icon>
+        Edit
+      </v-btn>
+    </v-row>
+    <div class="clear"></div>
     <v-list>
       <v-list-item v-for="item in items" :key="item.title">
+        <!-- name -->
         <v-list-item-content>
-          <router-link to="/about" class="link">
-            <v-list-item-title
-              v-text="item.title"
-              class="link-item"
-            ></v-list-item-title>
-          </router-link>
+          <v-list-item-title
+            v-text="item.title"
+            class="link-item"
+          ></v-list-item-title>
         </v-list-item-content>
-
+        <!-- pic -->
         <v-list-item-avatar>
           <v-img :src="item.avatar"></v-img>
         </v-list-item-avatar>
+        <!-- delete btn -->
+        <v-btn class="mx-2" fab dark small color="primary" v-if="edit">
+          <v-icon dark>
+            mdi-minus
+          </v-icon>
+        </v-btn>
       </v-list-item>
     </v-list>
   </v-card>
@@ -23,6 +42,7 @@
 export default {
   data() {
     return {
+      edit: false,
       items: [
         {
           icon: false,
@@ -52,19 +72,21 @@ export default {
     };
   },
   methods: {
-    isStar(item) {
-      item.icon = !item.icon;
-      if (item.icon) {
-        item.color = "pink";
-      } else {
-        item.color = "gray";
-      }
+    isEdit() {
+      this.edit = !this.edit;
     }
   }
 };
 </script>
 
 <style scoped lang="scss">
+.v-row {
+  float: right;
+  margin-right: 0px;
+}
+.clear {
+  clear: right;
+}
 .icon-num {
   margin-right: 10px;
   background-color: rgb(131, 131, 131, 0.3);
